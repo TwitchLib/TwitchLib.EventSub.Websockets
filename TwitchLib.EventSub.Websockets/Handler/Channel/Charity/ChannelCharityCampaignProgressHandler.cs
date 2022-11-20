e561e8e3a200ca7086b09029a133545983d894f5
Tobias "Syzuna" Teske <syzuna@syzuna-programs.de>
2022-11-21 00:46:04 +0100

    Added Handlers for new Charity SubscriptionTypes

---
+++ TwitchLib.EventSub.Websockets/Handler/Channel/Charity/ChannelCharityCampaignProgressHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.Json;
+using TwitchLib.EventSub.Core.SubscriptionTypes.Channel;
+using TwitchLib.EventSub.Websockets.Core.EventArgs;
+using TwitchLib.EventSub.Websockets.Core.EventArgs.Channel;
+using TwitchLib.EventSub.Websockets.Core.Handler;
+using TwitchLib.EventSub.Websockets.Core.Models;
+
+namespace TwitchLib.EventSub.Websockets.Handler.Channel.Charity
+{
+    /// <summary>
+    /// Handler for 'channel.charity_campaign.progress' notifications
+    /// </summary>
+    public class ChannelCharityCampaignProgressHandler : INotificationHandler
+    {
+        /// <inheritdoc />
+        public string SubscriptionType => "channel.charity_campaign.progress";
+
+        /// <inheritdoc />
+        public void Handle(EventSubWebsocketClient client, string jsonString, JsonSerializerOptions serializerOptions)
+        {
+            try
+            {
+                var data = JsonSerializer.Deserialize<EventSubNotification<ChannelCharityCampaignProgress>>(jsonString.AsSpan(), serializerOptions);
+
+                if (data is null)
+                    throw new InvalidOperationException("Parsed JSON cannot be null!");
+
+                client.RaiseEvent("ChannelCharityCampaignProgress", new ChannelCharityCampaignProgressArgs { Notification = data });
+            }
+            catch (Exception ex)
+            {
+                client.RaiseEvent("ErrorOccurred", new ErrorOccuredArgs { Exception = ex, Message = $"Error encountered while trying to handle {SubscriptionType} notification! Raw Json: {jsonString}" });
+            }
+        }
+    }
+}
