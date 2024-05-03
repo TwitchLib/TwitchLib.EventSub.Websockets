3bdc7f485e29a07c6d34a111347187ae188d10e6
AoshiW <Aoshi.W@gmail.com>
2024-05-03 19:04:21 +0200

    add json configuration

--- TwitchLib.EventSub.Websockets.Example.NetStandard/Program.cs
+++ TwitchLib.EventSub.Websockets.Example.NetStandard/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 using TwitchLib.EventSub.Websockets.Extensions;
 
 namespace TwitchLib.EventSub.Websockets.Example.NetStandard
@@ -9,6 +11,8 @@ namespace TwitchLib.EventSub.Websockets.Example.NetStandard
         private static void Main(string[] args)
         {
             var builder = Host.CreateApplicationBuilder(args);
+
+            builder.Configuration.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json"));
 
             //Add services to the container.
             builder.Services.AddLogging();
