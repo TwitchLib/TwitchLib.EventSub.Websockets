f421812630d3d5181d9d49ee7a423dc2f256504b
koishibuh <elysiagriffin@gmail.com>
2024-07-31 01:43:29 +0100

    Added ChannelModerate Handler & Args

---
+++ TwitchLib.EventSub.Websockets/Core/EventArgs/Channel/ChannelModerateArgs.cs
@@ -0,0 +1,12 @@
+﻿using TwitchLib.EventSub.Core.SubscriptionTypes.Channel;
+using TwitchLib.EventSub.Websockets.Core.Models;
+
+namespace TwitchLib.EventSub.Websockets.Core.EventArgs.Channel;
+
+/// <summary>
+/// <see href="https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types/#channelmoderate-v2">Twitch Documentation</see><br/>
+/// When a moderator performs a moderation action in a channel.<br/>
+/// Required Scopes: Check documenation for full list<br/>
+/// </summary>
+public class ChannelModerateArgs : TwitchLibEventSubEventArgs<EventSubNotification<ChannelModerate>>
+{ }
