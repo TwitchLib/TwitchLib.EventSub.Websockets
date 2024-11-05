e78808b88c6a3156feef9585b4f497e514f0a070
Proddy <josh@zhbot.org>
2024-11-05 17:41:18 +0000

    Add `Channel Warning` related handlers

---
+++ TwitchLib.EventSub.Websockets/Handler/Channel/Warning/ChannelWarningAcknowledgeHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.Json;
+using TwitchLib.EventSub.Core.SubscriptionTypes.Channel;
+using TwitchLib.EventSub.Websockets.Core.EventArgs;
+using TwitchLib.EventSub.Websockets.Core.EventArgs.Channel;
+using TwitchLib.EventSub.Websockets.Core.Handler;
+using TwitchLib.EventSub.Websockets.Core.Models;
+
+namespace TwitchLib.EventSub.Websockets.Handler.Channel.Warning
+{
+    /// <summary>
+    /// Handler for 'channel.warning.acknowledge' notifications
+    /// </summary>
+    public class ChannelWarningAcknowledgeHandler : INotificationHandler
+    {
+        /// <inheritdoc />
+        public string SubscriptionType => "channel.warning.acknowledge";
+
+        /// <inheritdoc />
+        public void Handle(EventSubWebsocketClient client, string jsonString, JsonSerializerOptions serializerOptions)
+        {
+            try
+            {
+                var data = JsonSerializer.Deserialize<EventSubNotification<ChannelWarningAcknowledge>>(jsonString.AsSpan(), serializerOptions);
+
+                if (data is null)
+                    throw new InvalidOperationException("Parsed JSON cannot be null!");
+
+                client.RaiseEvent("ChannelWarningAcknowledge", new ChannelWarningAcknowledgeArgs { Notification = data });
+            }
+            catch (Exception ex)
+            {
+                client.RaiseEvent("ErrorOccurred", new ErrorOccuredArgs { Exception = ex, Message = $"Error encountered while trying to handle {SubscriptionType} notification! Raw Json: {jsonString}" });
+            }
+        }
+    }
+}
