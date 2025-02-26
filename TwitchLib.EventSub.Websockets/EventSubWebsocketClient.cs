c3c2f758ffdf4de36eeb1461db5c87b212a4737e
Iris <68797873+iryis@users.noreply.github.com>
2025-02-26 04:21:17 -0500

    Remove chat notification (see 51)

--- TwitchLib.EventSub.Websockets/EventSubWebsocketClient.cs
+++ TwitchLib.EventSub.Websockets/EventSubWebsocketClient.cs
@@ -310,11 +310,6 @@ namespace TwitchLib.EventSub.Websockets
         /// Event that triggers on "channel.shared_chat.end" notifications
         /// </summary>
         public event AsyncEventHandler<ChannelSharedChatSessionEndArgs> ChannelSharedChatSessionEnd;
-        
-        /// <summary>
-        /// Event that triggers on "channel.chat.notification" notifications
-        /// </summary>
-        public event AsyncEventHandler<ChannelChatNotificationArgs> ChannelChatNotification;
 
         #endregion
 
